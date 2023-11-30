a171f8037725e2346166e6f881349d3030be5feb
Rahim <103603909+RahimMammad44@users.noreply.github.com>
2023-11-30 08:30:14 +0400

    added Tailwind

--- coffee/src/pages/Shop/index.jsx
+++ coffee/src/pages/Shop/index.jsx
@@ -2,30 +2,20 @@ import React from 'react'
 import "./index.scss"
 import useFetchData from '../../hooks/UseFetchData'
 import ProductCard from '../../components/ProductCard';
+import Product from '../../components/Product';
 
 const Shop = () => {
-  const { data, isLoading, error } = useFetchData("products");
-
 
   return (
-    <div>
+    <div className='max-w-[90%] mx-auto'>
       <div className='contact-main'>
             <h2>CAFENA PRODUCT</h2>
             <div className='home-slash-page'>
               <h6>HOME/</h6>
               <span>PRODUCT</span>
             </div>
-        </div>
-        <div className='products-main-container'>
-          <div className="grid-sort-container"></div>
-              {
-                data && data.map((product) => {
-                  return (
-                    <ProductCard key={product.id} product={product} />
-                  )
-                })
-              }
-        </div>
+      </div>
+      <Product />
     </div>
   )
 }
