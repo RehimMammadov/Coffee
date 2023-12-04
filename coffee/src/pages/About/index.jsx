10add2efe07e708ac3b06fb3fa2f702f193c6d98
Rahim <103603909+RahimMammad44@users.noreply.github.com>
2023-12-04 09:03:32 +0400

    About Done

--- coffee/src/pages/About/index.jsx
+++ coffee/src/pages/About/index.jsx
@@ -4,24 +4,38 @@ import BestHouse from '../../components/AboutPageBestHouse'
 import BestService from '../../components/AboutPageBestService'
 import HousingCity from '../../components/AboutPageHousingCity'
 import Professionals from '../../components/AbooutPageProffessionals'
+import Loading from '../isLoading'
+import Error from '../Error'
+import useFetchData from '../../hooks/UseFetchData'
 
 
 const About = () => {
+  const { data, isLoading, error } = useFetchData("contact");
   return (
-    <section id='about'>
-      <div className='about-header'>
-        <div className="bg"></div>
-        <div className='about-head-texts'>
-          <p>ABOUT CAFENA</p>
-          <h3>HOME / <span>ABOUT US</span></h3>
-        </div>
-        <img className='shape' src="https://xpressrow.com/html/cafena/cafena/assets/images/shape/breadcrumb-shape-2.png" alt="" />
-      </div>
-      <BestHouse/>
-      <BestService/>
-      <HousingCity/>
-      <Professionals/>
-    </section>
+    <>
+      {
+        isLoading ? (
+          <Loading />
+        ) : error ? (
+          <Error />
+        ) : (
+          <section id='about'>
+              <div className='about-header'>
+                <div className="bg"></div>
+                <div className='about-head-texts'>
+                  <p>ABOUT CAFENA</p>
+                  <h3>HOME / <span>ABOUT US</span></h3>
+                </div>
+                <img className='shape' src="https://xpressrow.com/html/cafena/cafena/assets/images/shape/breadcrumb-shape-2.png" alt="" />
+              </div>
+              <BestHouse/>
+              <BestService/>
+              <HousingCity/>
+              <Professionals/>
+          </section>
+        )
+      }
+    </>
   )
 }
 
